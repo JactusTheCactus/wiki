1cfc052fd306e317872a971c3410479282a539e3
JactusTheCactus <threateninglyamusing@gmail.com>
2025-03-12 18:16:00 -0700

    Update sankey.py

--- worldbuilding/fantasy/mythology/sankey.py
+++ worldbuilding/fantasy/mythology/sankey.py
@@ -11,7 +11,7 @@ fig = go.Figure(go.Sankey(
         x=[0.2,0.2,0.2,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
         customdata=descriptionList,
         hovertemplate='<b>%{label}</b><br><i>%{customdata}</i><extra></extra>',
-        align='left'
+        align='justify'
 
     ),
     link=dict(
