a354b183742708211cfc23ad358f5e4c9563c109
JactusTheCactus <threateninglyamusing@gmail.com>
2025-04-01 23:59:51 -0700

    Update script.js

--- worldbuilding/fantasy/concordium/script.js
+++ worldbuilding/fantasy/concordium/script.js
@@ -7,7 +7,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Output the data to the HTML
             const outputDiv = document.getElementById("output");
-            outputDiv.innerHTML = `<h1>${jsonData.name} ${jsonData.rank}</h1><h2>The ${jsonData.animal} ${jsonData.alignment} of ${jsonData.aspect}</h2><h3>"${jsonData.epithet}"</h3><ul><li>${jsonData.colour}</li><li>${jsonData.weapon}</li><li>${jsonData.power}</li><li>${jsonData.species}</li></ul><p>${jsonData.description}</p>`;
+            outputDiv.innerHTML = `
+<h1>${jsonData.name} ${jsonData.rank}</h1>
+<h2>The ${jsonData.animal} ${jsonData.alignment} of ${jsonData.aspect}</h2>
+<h3>"${jsonData.epithet}"</h3>
+<ul>
+  <li><b>Armour Colour:</b> ${jsonData.colour}</li>
+  <li><b>Weapon:</b> ${jsonData.weapon}</li>
+  <li><b>Power:</b> ${jsonData.power}</li>
+  <li><b>Species:</b> ${jsonData.species}</li>
+</ul>
+<p>${jsonData.description}</p>
+`;
         } catch (error) {
             console.error("Error parsing JSON:", error);
         }
