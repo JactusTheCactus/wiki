0f4e1025ae51f342bd3c1aa5f24cc67cd3bb8c93
JactusTheCactus <threateninglyamusing@gmail.com>
2025-04-03 00:16:34 +0000

    -

--- worldbuilding/fantasy/concordium/script.js
+++ worldbuilding/fantasy/concordium/script.js
@@ -8,11 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
             // Output the data to the HTML
             const outputDiv = document.getElementById("output");
             outputDiv.innerHTML = `
-${jsonData.name ? `<h1>${[jsonData.name,jsonData.rank].filter(Boolean).join(' ')}</h1>` : ''}
+${jsonData.name ? `<h1>${[jsonData.name, jsonData.rank].filter(Boolean).join(' ')}</h1>` : ''}
 ${jsonData.epithet ? `<h3><i>"${jsonData.epithet}"</i></h3>` : ''}
-${`<h2>The ${[jsonData.animal,jsonData.alignment].filter(Boolean).join(' ')}${jsonData.aspect ? ` of ${jsonData.aspect}` : ''}</h2>`}
+${`<h2>The ${[jsonData.animal, jsonData.alignment].filter(Boolean).join(' ')}${jsonData.aspect ? ` of ${jsonData.aspect}` : ''}</h2>`}
 ${(jsonData.colour || jsonData.weapon || jsonData.power || jsonData.species) ?
-`
+                    `
 <ul>
   ${jsonData.colour ? `<li><b>Armour Colour:</b> ${jsonData.colour}</li>` : ''}
   ${jsonData.weapon ? `<li><b>Weapon:</b> ${jsonData.weapon}</li>` : ''}
